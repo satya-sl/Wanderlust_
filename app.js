9e2991010046beb3d8d42d183dbc7b20f77bef1e
satya-sl <satya.64444.com>
2024-08-13 14:33:37 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {listingSchema , reviewSchema} = require('./schema.js')
 const Review = require("./models/review.js");
 
 
-const listings = require("./routes/listing.js");
+//const listings = require("./routes/listing.js");
 
 
 
@@ -59,7 +59,89 @@ const validateReview = (req,res,next)=>{
 
 
 //    ------------------ Listings ---------------------
-app.use("/listings",listings);
+
+// validation for schema as function use it as "middleWare"
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(","); 
+        throw new ExpressError(400 ,errMsg)
+    }
+    else{
+        next()
+    }
+}
+
+
+
+//index route
+app.get("/listings/",async (req,res)=>{
+    const allListings =  await  Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+});
+
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+})
+//create route
+app.post("/listings", validateListing, wrapAsync(async(req,res,next) =>{
+    //let {title ,description ,image,price,country,location} = req.body;   1 way u can use objec too
+    //let listing = req.body.listing ;       //like this making object ,"listing[title]"
+    
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    console.log(newListing);
+    res.redirect("listings");
+}))
+
+/*      try-catch block example
+app.post("/listings", async(req,res,next) =>{
+    try{
+        //let {title ,description ,image,price,country,location} = req.body;   1 way u can use objec too
+        //let listing = req.body.listing ;       //like this making object ,"listing[title]"
+        let newListing = new Listing(req.body.listing);
+        await newListing.save();
+        console.log(newListing);
+        res.redirect("/listings");
+    }catch(err){
+        next(err);
+    }
+})*/
+
+
+//edit route
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
+    let {id} = req.params;   //extracting id
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing})
+}))
+
+//update route
+app.put("/listings/:id", validateListing,wrapAsync(async(req,res)=>{
+    let {id}= req.params;
+  await  Listing.findByIdAndUpdate(id,{...req.body.listing});
+  //res.redirect("/listings");
+  res.redirect(`/listings/${id}`); // takes to show(specific listing ) after editing submition
+}))
+
+
+//show route , shows specific listing data(view)  , for this we already changed in index.js to get "id"
+app.get("/listings/:id", wrapAsync( async (req,res)=>{
+    let {id} = req.params;   //extracting id
+    const listing = await Listing.findById(id).populate("reviews");
+    console.log(listing);
+    res.render("listings/show.ejs",{listing});
+}));
+
+//delete route
+app.delete("/listings/:id", wrapAsync(async (req,res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("listings");
+}))
+
 
 
 
